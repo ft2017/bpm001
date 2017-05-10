79122a2bb3e2806416aa152a3d635e64da967bc7
unknown <yx.zhang@fulltech-metal.com>
2017-05-10 17:05:50 +0800

    上传2017-05-09内容

---
+++ zyx张一翔/bpm.41/SOP_张一翔/SOP002_工单制成变更/ft002 工单制程变更.js
@@ -0,0 +1,38 @@
+﻿
+document.write('<script type="text/javascript" src="../../CustomJsLib/EFGPShareMethod.js"></script>');//for 开窗
+
+
+var tDbConn = new DataSource("SOP002","SQL0");//前面是表单代号，后面是sql command控件的名称
+var tEFGP = "T100PROD"; 
+
+
+
+function formCreate(){
+return true;
+}
+function formOpen(){
+return true;
+}
+function formSave(){
+return true;
+}
+function formClose(){
+return true;
+}
+
+
+function Button22_onclick(){
+	var FileName = "SingleOpenWin";
+   	var sql = " select a.sfaadocno 工单号,a.sfaa010 生产料号,b.imaal003 品名,a.sfaa012 生产数量 from sfaa_t a left join imaal_t b on b.imaal001=a.sfaa010 and b.imaalent=11 where a.sfaaent=11 order by a.sfaadocno";
+
+	var SQLClaused = new Array(sql);
+	var SQLLabel = new Array("工单单号","生产料号","品名","数量");//客製開窗的Grid Label
+	var QBEField = new Array("a.sfaadocno","a.sfaa010","b.imaal003","a.sfaa012");//模糊查詢,須和DB Table欄位名稱相同
+	var QBELabel = new Array("工单单号","生产料号","品名","数量");//模糊查詢的Label
+     var ReturnId = new Array("Textbox21", "Textbox24", "Textbox26","Textbox30");
+     singleOpenWin(FileName, tEFGP, SQLClaused, SQLLabel, QBEField, QBELabel, ReturnId, 720, 430);
+}
+
+
+
+//$-----Auto generated script block, Please do not edit or modify script below this line.-----$//
---
+++ zyx张一翔/bpm.41/zyx张一翔/zyx001/zyx001.js
@@ -0,0 +1,66 @@
+﻿function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + date.getHours() + seperator2 + date.getMinutes()
+            + seperator2 + date.getSeconds();
+    return currentdate;
+} 
+
+
+function formCreate(){
+	document.getElementById("txtUserId01").value = userId;
+    document.getElementById("txtUserName01").value = userName;
+	//document.getElementById("Textbox1").value = mainOrgUnitIds;
+	//document.getElementById("Textbox2").value = mainOrgUnitNames;
+	//document.getElementById("tbrq").value=systemDateTime;
+return true;
+}
+function formOpen(){
+if(activityId == "UserTask_3" ){
+	document.getElementById("txtUserId02").value = userId;
+    document.getElementById("txtUserName02").value = userName;
+}
+if(activityId == "UserTask_8" ){
+	document.getElementById("txtUserId03").value = userId;
+    document.getElementById("txtUserName03").value = userName;
+}
+return true;
+}
+function formSave(){
+var now=new Date();
+var year=now.getYear();
+var month=now.getMonth();
+var day=now.getDate();
+var hours=now.getHours();
+var minutes=now.getMinutes();
+//var seconds=now.getSeconds();
+//document.getElementById("Teb003").value=""+year+"/"+month+"/"+day+" "+hours+":"+minutes+""; 
+var t=getNowFormatDate();
+if(activityId == "UserTask_2" ){
+	document.getElementById("txtTime01").value = t;
+    //document.getElementById("txtUserName02").value = userName;
+}
+if(activityId == "UserTask_3" ){
+	document.getElementById("txtTime02").value = t;
+    //document.getElementById("txtUserName02").value = userName;
+}
+if(activityId == "UserTask_8" ){
+	document.getElementById("txtTime03").value = t;
+  //  document.getElementById("txtUserName03").value = userName;
+}
+return true;
+}
+function formClose(){
+return true;
+}
+//$-----Auto generated script block, Please do not edit or modify script below this line.-----$//
