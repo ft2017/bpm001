88e0b6f1abe7de8f51cab94a2f1c974d7af3cb79
unknown <yx.zhang@fulltech-metal.com>
2017-05-25 15:56:40 +0800

    05-25，张一翔，T100表单上传

---
+++ zyx张一翔/bpm.41/T100/料件申請維護作業(aimt300).js
@@ -0,0 +1,27 @@
+﻿function formCreate(){
+    return true;
+}
+function formOpen(){
+    /********Activity Reload Grid Script********/
+
+    /********Set Grid Width Script********/
+
+    /********NoBorder TextBox Script********/
+    document.getElementById("imba900_desc").style.border = "none";
+    document.getElementById("imba901_desc").style.border = "none";
+    document.getElementById("imba009_desc").style.border = "none";
+    document.getElementById("imba003_desc").style.border = "none";
+    document.getElementById("imba005_desc").style.border = "none";
+    document.getElementById("imba006_desc").style.border = "none";
+    document.getElementById("imba010_desc").style.border = "none";
+    document.getElementById("imba142_desc").style.border = "none";
+
+    return true;
+}
+function formSave(){
+    return true;
+}
+function formClose(){
+    return true;
+}
+//$-----Auto generated script block, Please do not edit or modify script below this line.-----$//
